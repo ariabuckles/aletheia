0411e33ca889a30fe83e16cb07adfd603be3a801
Aria Buckles <aria@toole1.com>
2014-04-10 18:32:37 -0700

    compiling, getting closer
    test plan: none

--- src/parser-generator.js
+++ src/parser-generator.js
@@ -67,24 +67,37 @@ var grammar = {
         ],
         "statement": [
             ["NEWLINE", ""],  // discard
-            ["statementBody NEWLINE", "$$ = $1"]
+            ["statementBody NEWLINE", "$$ = $1;"]
         ],
         "statementBody": [
             ["IDENTIFIER = expression", "return new yy.Declaration(false, $1, $3);"],
             ["VARKEYWORD IDENTIFIER = expression", "return new yy.Declaration(true, $2, $4);"],
-            ["MUTATE lvalue = expression", "return new yy.Mutation($2, $3)"],
+            ["MUTATE lvalue = expression", "return new yy.Mutation($2, $3);"],
+            ["functionCall", "$$ = $1;"]
         ],
         "expression": [
-            ["lvalue", "$$ = $1"],
-            ["rvalue", "$$ = $1"]
+            ["rvalue", "$$ = $1;"]
+        ],
+        "rvalue": [
+            ["functionCall", "$$ = $1;"],
+            ["unitExpression", "$$ = $1;"],
+        ],
+        "unitExpression": [
+            ["( expression )", "$$ = [$1];"],
+            ["function", "$$ = $1;"],
+            ["literal", "$$ = $1;"],
+            ["lvalue", "$$ = $1"]
         ],
         "lvalue": [
             ["IDENTIFIER", "$$ = $1"],
 //            ["tableaccess", "$$ = $1"]
         ],
-        "rvalue": [
-            ["function", "$$ = $1;"],
-            ["literal", "$$ = $1;"]
+        "functionCall": [
+            ["IDENTIFIER args", "return new yy.FunctionCall($1, $2);"]
+        ],
+        "args": [
+            ["unitExpression", "$$ = [$1];"],
+            ["args unitExpression", "$$ = $1; $1.push($2);"]
         ],
 //        "tableaccess": [
 //
@@ -107,67 +120,24 @@ var grammar = {
 //        ],
         "function": [
             ["[ statementList ]", "$$ = new yy.Function([], $2);"],
-            ["[ argList | statementList ]", "$$ = new yy.Function($2, $4);"]
+//            ["[ argList | statementList ]", "$$ = new yy.Function($2, $4);"]
         ],
-        "additive": [
-            ["additive + multiplicative", "$$ = yy.Add.createOrAppend($1, $3);"],
-            ["additive - multiplicative", "$$ = yy.Add.createOrAppend($1, yy.Mul.handleNegative($3, \"subtract\"));"],
-            ["multiplicative", "$$ = $1;", {prec: "+"}]
-        ],
-        "multiplicative": [
-            // the second term in an implicit multiplication cannot be negative
-            ["multiplicative triglog", "$$ = yy.Mul.fold(yy.Mul.createOrAppend($1, $2));"],
-            ["multiplicative * negative", "$$ = yy.Mul.fold(yy.Mul.createOrAppend($1, $3));"],
-            ["multiplicative / negative", "$$ = yy.Mul.fold(yy.Mul.handleDivide($1, $3));"],
-            ["negative", "$$ = $1;"]
-        ],
-        "negative": [
-            ["- negative", "$$ = yy.Mul.handleNegative($2);", {prec: "UMINUS"}],
-            ["triglog", "$$ = $1;"]
-        ],
-        "trig": [
-            ["TRIG", "$$ = [yytext];"]
-        ],
-        "trigfunc": [
-            ["trig", "$$ = $1;"],
-            ["trig ^ negative", "$$ = $1.concat($3);"],
-            ["TRIGINV", "$$ = [yytext];"]
-        ],
-        "logbase": [
-            ["ln", "$$ = yy.Log.natural();"],
-            ["log", "$$ = yy.Log.common();"],
-            ["log _ subscriptable", "$$ = $3;"]
-        ],
-        "triglog": [
-            ["trigfunc negative", "$$ = yy.Trig.create($1, $2);"],
-            ["logbase negative", "$$ = yy.Log.create($1, $2);"],
-            ["power", "$$ = $1;"]
-        ],
-        "power": [
-            ["primitive ^ negative", "$$ = new yy.Pow($1, $3);"],
-            ["primitive", "$$ = $1;"]
-        ],
-        "variable": [
-            ["VAR", "$$ = yytext;"]
-        ],
-        "subscriptable": [
-            ["variable _ subscriptable", "$$ = new yy.Var($1, $3);"],
-            ["variable", "$$ = new yy.Var($1);"],
-            ["CONST", "$$ = new yy.Const(yytext.toLowerCase());"],
-            ["INT", "$$ = yy.Int.create(Number(yytext));"],
-            ["FLOAT", "$$ = yy.Float.create(Number(yytext));"],
-            ["( additive )", "$$ = $2.completeParse().addHint('parens');"] // this probably shouldn't be a hint...
-        ],
-        "invocation": [
-            ["sqrt ( additive )", "$$ = yy.Pow.sqrt($3);"],
-            ["abs ( additive )", "$$ = new yy.Abs($3);"],
-            ["| additive |", "$$ = new yy.Abs($2);"],
-            ["function ( additive )", "$$ = new yy.Func($1, $3);"]
-        ],
-        "primitive": [
-            ["subscriptable", "$$ = $1;"],
-            ["invocation", "$$ = $1;"]
-        ]
+//        "additive": [
+//            ["additive + multiplicative", "$$ = yy.Add.createOrAppend($1, $3);"],
+//            ["additive - multiplicative", "$$ = yy.Add.createOrAppend($1, yy.Mul.handleNegative($3, \"subtract\"));"],
+//            ["multiplicative", "$$ = $1;", {prec: "+"}]
+//        ],
+//        "multiplicative": [
+//            // the second term in an implicit multiplication cannot be negative
+//            ["multiplicative triglog", "$$ = yy.Mul.fold(yy.Mul.createOrAppend($1, $2));"],
+//            ["multiplicative * negative", "$$ = yy.Mul.fold(yy.Mul.createOrAppend($1, $3));"],
+//            ["multiplicative / negative", "$$ = yy.Mul.fold(yy.Mul.handleDivide($1, $3));"],
+//            ["negative", "$$ = $1;"]
+//        ],
+//        "negative": [
+//            ["- negative", "$$ = yy.Mul.handleNegative($2);", {prec: "UMINUS"}],
+//            ["triglog", "$$ = $1;"]
+//        ],
     }
 };
 
